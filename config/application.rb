a14c1b1da6fc5200c8c311e11a518cfbf75266f9
Charlie <sgvvannabe@gmail.com>
2016-02-15 09:35:57 +0000

    Fix the squirrel font problem on heroku

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Splurty
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf)
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
     
   end
 end
